10f81b33c7efd5ff05affb7574719f039aba961c
Hong Quan <quandh619@gmail.com>
2020-06-04 22:33:59 +0700

    The media use the absolute path

--- src/AStarGraph/MainApp.java
+++ src/AStarGraph/MainApp.java
@@ -1,22 +1,34 @@
 package AStarGraph;
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.layout.StackPane;
 import javafx.stage.*;
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class MainApp extends Application  {
 	
 	private Stage primaryStage;
 	private StackPane root;
-	
+	private MediaPlayer player;
 	
 	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception{
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AStarSearch");
+		
+		String path = "C:\\Users\\quan.dh176850\\eclipse-workspace\\AStarPathFinding\\music\\BoJack-Horseman-Theme-Song.mp3";
+		Media media = new Media(new File(path).toURI().toString());
+		
+		player = new MediaPlayer(media);
+		player.play();
+		
+		
 		showApp();
 	}
 	
