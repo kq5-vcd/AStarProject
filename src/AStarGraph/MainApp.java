252c16b394e7fffff247b9fb3640093819b88f6d
DuyCVu <duy822.kq5@gmail.com>
2020-06-05 09:36:34 +0700

    Source modified

--- src/AStarGraph/MainApp.java
+++ src/AStarGraph/MainApp.java
@@ -3,18 +3,18 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.*;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.stage.*;
 import java.io.File;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
 public class MainApp extends Application  {
 	
 	private Stage primaryStage;
 	private StackPane root;
-	private MediaPlayer player;
+//	private MediaPlayer player;
 	
 	
 	@Override
@@ -22,30 +22,41 @@ public class MainApp extends Application  {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AStarSearch");
 		
-		String path = "C:\\Users\\quan.dh176850\\eclipse-workspace\\AStarPathFinding\\music\\BoJack-Horseman-Theme-Song.mp3";
-		Media media = new Media(new File(path).toURI().toString());
+//		String path = "C:\\Users\\quan.dh176850\\eclipse-workspace\\AStarPathFinding\\music\\BoJack-Horseman-Theme-Song.mp3";
+//		Media media = new Media(new File(path).toURI().toString());
 		
-		player = new MediaPlayer(media);
-		player.play();
+//		player = new MediaPlayer(media);
+//		player.play();
 		
 		
-		showApp();
-	}
-	
-	public void showApp() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(DisplayGraph.class.getResource("AStarFrame.fxml"));
 			root = (StackPane) loader.load();
 			
 			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			this.primaryStage.setScene(scene);
+			this.primaryStage.show();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+//	public void showApp() {
+//		try {
+//			FXMLLoader loader = new FXMLLoader();
+//			loader.setLocation(DisplayGraph.class.getResource("AStarFrame.fxml"));
+//			root = (StackPane) loader.load();
+//			
+//			Scene scene = new Scene(root);
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	public static void main(String[] args) {
 		launch(args);
